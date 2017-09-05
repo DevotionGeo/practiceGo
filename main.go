7b81fd8d92a5acca3a388b3388e37872ec42fc7a
Arif <arifhussain.33@gmail.com>
2017-09-05 12:00:34 +0500

    modified main and binary

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Welcome to my website!</h1>")
+	fmt.Fprint(w, "<h1>Welcome to Whatever!</h1>")
 }
 
 func main() {
